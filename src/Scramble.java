8ba8a5c1e620e25545f02620168732eb959ee848
elhm88 <elhm88@student.cis.edu.hk>
2024-08-23 10:15:07 +0800

    First commit.

---
+++ src/Scramble.java
@@ -0,0 +1,62 @@
+import java.util.*;
+public class Scramble {
+    public String original = "";
+
+    public Scramble(String original) {
+        this.original = original;
+    }
+
+    // Helper method to swap letters
+    private static String swapLetters(String toSwap) { // Remember to insert substring of 'original'
+        char a = toSwap.charAt(0);
+        char b = toSwap.charAt(1);
+        String swapped = "" + b + a;
+        return swapped;
+    }
+
+    public static String scrambleWord(String original) {
+        String scrambled = "";
+
+        // og: ABRACADABRA
+        // i = xxxxxxxxxx
+        // re: BARCADABA
+
+        for (int i = 0; i < original.length() - 2; i++) {
+            if (original.charAt(i) == 'A' && original.charAt(i + 1) != 'A') {
+                scrambled += swapLetters(original.substring(i, i + 2));
+                i++;
+            } else if (original.charAt(i) != 'A' && original.charAt(i + 1) == 'A' && original.charAt(i + 2) != 'A') {
+                scrambled += original.charAt(i) + swapLetters(original.substring(i + 1, i + 3));
+                i += 2;
+            } else {
+                scrambled += original.charAt(i);
+            }
+        }
+
+        if (scrambled.length() == original.length() - 1) {
+            return scrambled + original.charAt(original.length() - 1);
+        } else if (scrambled.length() == original.length() - 2) {
+            return scrambled + original.substring(original.length() - 2, original.length());
+        } else {
+            return scrambled;
+        }
+    }
+
+    public static List<String> scrambleOrRemove(List<String> original) {
+        List<String> wordList = new ArrayList<String>();
+
+        // Add scrambled words to wordList
+        for (String x : original) {
+            if (! x.equals(scrambleWord(x))) {
+                wordList.add(scrambleWord(x));
+            }
+        }
+
+        // Debug :(
+        for (String x : wordList) {
+            System.out.println(x);
+        }
+
+        return wordList;
+    }
+}
